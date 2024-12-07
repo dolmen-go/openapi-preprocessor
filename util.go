32249898e1fca1bd7f9f184e0772ec828978b31a
Olivier Mengué <dolmen@cpan.org>
2024-12-07 22:03:40 +0100

    util: add iterPaths, iterOperations, iterSecurity

--- util.go
+++ util.go
@@ -4,6 +4,8 @@ import (
 	"iter"
 	"sort"
 	"strconv"
+
+	"github.com/dolmen-go/jsonptr"
 )
 
 func sortedKeys(obj map[string]interface{}) (keys []string) {
@@ -60,6 +62,80 @@ func iterArrayPtr[T any](ptr string, arr []any) iter.Seq2[string, T] {
 					return
 				}
 			}
+		}
+	}
+}
+
+func seq2Noop[K any, V any](yield func(K, V) bool) {
+	return
+}
+
+func iterPaths(root any) iter.Seq2[string, map[string]any] {
+	pathsAny, err := jsonptr.Get(root, `/paths`)
+	if err != nil {
+		return seq2Noop[string, map[string]any]
+	}
+	paths, ok := pathsAny.(map[string]any)
+	if !ok {
+		return seq2Noop[string, map[string]any]
+	}
+
+	return func(yield func(string, map[string]any) bool) {
+		for path, pathSpec := range paths {
+			if p, ok := pathSpec.(map[string]any); ok {
+				if len(p) == 0 {
+					continue
+				}
+				if !yield(`/paths/`+jsonptr.EscapeString(path), p) {
+					return
+				}
+			}
+		}
+	}
+}
+
+var methods = [...]bool{
+	'g' + 'e' + 't': true, // get
+	'p' + 'u' + 't': true, // put
+	'p' + 'o' + 's': true, // post
+	'd' + 'e' + 'l': true, // delete
+	'o' + 'p' + 't': true, // options
+	'h' + 'e' + 'a': true, // head
+	'p' + 'a' + 't': true, // patch
+	't' + 'r' + 'a': true, // trace
+}
+
+func iterOperations(root any) iter.Seq2[string, map[string]any] {
+	return func(yield func(string, map[string]any) bool) {
+		for ptr, spec := range iterPaths(root) {
+			for k, opAny := range spec {
+				if len(k) < 3 {
+					continue
+				}
+				kk := int(k[0]) + int(k[1]) + int(k[2])
+				if kk < len(methods) && methods[kk] {
+					if op, ok := opAny.(map[string]any); ok {
+						if !yield(ptr+"/"+jsonptr.EscapeString(k), op) {
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func iterSecurity(ptr string, doc map[string]any) iter.Seq2[string, map[string]any] {
+	return func(yield func(string, map[string]any) bool) {
+		if opSec, hasSec := doc["security"].([]any); hasSec {
+			/*
+				for p, req := range iterArrayPtr[map[string]any](ptr, opSec) {
+					if !yield(p, req) {
+						return
+					}
+				}
+			*/
+			iterArrayPtr[map[string]any](ptr, opSec)(yield)
 		}
 	}
 }
